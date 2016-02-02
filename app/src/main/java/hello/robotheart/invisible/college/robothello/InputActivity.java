f5cb80fafb092e7fbb4dc12549946d6b087564c3
Paul Pham <paul@codefellows.com>
2016-02-01 22:20:22 -0700

    End of Session 1: starting up a new InputActivity, reading from an EditText, displaying from a button.

--- app/src/main/java/hello/robotheart/invisible/college/robothello/InputActivity.java
+++ app/src/main/java/hello/robotheart/invisible/college/robothello/InputActivity.java
@@ -2,6 +2,10 @@ package hello.robotheart.invisible.college.robothello;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class InputActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class InputActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
+
+        final Button button = (Button) findViewById(R.id.button);
+        final TextView textView = (TextView) findViewById(R.id.textView);
+        final EditText editText = (EditText) findViewById(R.id.editText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Hello " + editText.getText());
+            }
+        });
     }
 }
